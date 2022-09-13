6aff9181869be9937bc6766922ecfb86c955c643
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-09-12 17:26:05 -0700

    filted categories props to app.js, add state and useEffect

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import About from './components/About'
-import Nav from './components/Nav'
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
 
 function App() {
+  //lift the props to parent
+  //initializing the category state as an array of a few objects
+  //chose to use the useState hook here so that we can have the
+  //option to change the categories at some point in the future
+  const [categories] = useState([
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
-       
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -1,44 +1,41 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
+function Nav(props) {
   //define the categories in an array above the return statement.
   //Instead of just listing each category's name, we'll create objects that contain each category's name and a short description.
   //That way, we can use that same data elsewhere in the app.
-  const categories = [
-    {
-      name: "Comercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    {
-      name: "Portraits",
-      description: "Portraits of people in my life",
-    },
-    {
-      name: "Food",
-      description: "Delicious delicacies",
-    },
-    {
-      name: "Landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ];
 
-  //we've provided a function name to onClick(),
-  // we need to define it somewhere above the return statement
-  function categorySelected(name) {
-    console.log(`${name} clicked`);
-  }
+
+    const {
+      categories = [],
+      setCurrentCategory,
+      currentCategory,
+    } = props;
+
+  //use the useEffect Hook by invoking the function
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
+  //first argument is the callback function, and the second argument is an array with a 
+  //single element, currentCategory. The second argument directs the hook to re-render 
+  //the component on changes to the value of this state. In other words, 
+  //if currentCategory changes now, the component will re-render so that the change 
+  //in document.title will be visible to the use
 
   return (
-    <header className="flex-row">
+    <header className="flex-row px-1">
       <h2>
         {/* //adding the logo to the navigation and the About and Contact sections.  */}
         {/* //reason we'll use a separate data-testid attribute specific for testing purposes 
         //instead of using the id attribute is the same as why we don't query an element
         // by its class: to follow the best-practice principle of separating concerns. */}
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span>Oh Snap!
+          <span role="img" aria-label="camera">
+            {" "} 
+            📸
+            </span>{" "}
+            Oh Snap!
           </a>
       </h2>
       <nav>
@@ -52,8 +49,15 @@ function Nav() {
           {/* //map over the categories array*/}
           {/* Whenever we map over anything in JSX, the outermost element must have a key attribute 
           that's set to be something unique. This helps React keep track of items in the virtual DOM */}
-          {categories.map((category) => (
-            <li className="mx-1" key={category.name}>
+                 {categories.map((category) => (
+
+            //short-circuit expression
+            //currentCategory.name === category.name will get evaluated, and as long as it is true, 
+            //then the second bit of the short circuit, navActive, will be returned.
+            <li  className={`mx-1 ${
+              currentCategory.name === category.name && 'navActive'
+              }`}
+            key={category.name}>
               {/* //Note also the use of parentheses in the map callback to return JSX. When you map over 
                 //an array in a JSX expression, you should return only a single JSX element—like how you 
                 //can only return a single element from a React component. */}
@@ -61,8 +65,12 @@ function Nav() {
               {/* set up event listener to return category name when a category is clicked
                 we'll use the built-in onClick() method */}
               {/* //In that case, let's wrap the categorySelected reference in an anonymous arrow function */}
-              <span onClick={() => categorySelected(category.name)}>
-                {category.name}
+              <span
+                onClick={() => {
+                  setCurrentCategory(category)
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
               </span>
               {/* //The onClick() attribute is expecting a callback function declaration. 
               It's important that we wrap it in a function declaration rather than just calling categorySelected(category.name), 
